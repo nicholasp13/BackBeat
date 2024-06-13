795ca1f7723a16376dae5e092cd206189ec3a470
nicholasp13 <nicholasprado13@gmail.com>
2024-06-12 17:18:49 -0700

    Removed AudioFileReader as a class and made it into a namespace with free functions for better style/clarity

--- BackBeat/src/BackBeat/Audio/Helpers/AudioFileReader.h
+++ BackBeat/src/BackBeat/Audio/Helpers/AudioFileReader.h
@@ -3,13 +3,12 @@
 #include "BackBeat/Audio/Audio.h"
 namespace BackBeat {
 
-	class AudioFileReader
+	namespace AudioFileReader
 	{
-	public:
-		static AudioInfo ReadFile(std::string fileName);
+		AudioInfo ReadFile(std::string fileName);
 		
-	private:
 		static AudioInfo ReadMP3Header(std::string filePath, unsigned int size);
 		static AudioInfo ReadHeader(std::string filePath, unsigned int size, FileType type);
-	};
+	}
+
 }
